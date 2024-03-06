062b09d9bf16651b2a4a85d03ff8e7a39069c746
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-03-06 09:34:34 -0300

    desconto com map

--- notes.js
+++ notes.js
@@ -11,3 +11,13 @@ usando forEach "para cada item da lista, executa uma função"
 livros.forEach(livro => {
     console.log(livro)
 })
+
+função map 
+ex: 
+const array1 = [1, 4, 9, 16];
+
+//passe uma função para mapear
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+//expected output : array [2, 8, 18, 32]
