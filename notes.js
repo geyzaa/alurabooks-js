4eea575a21db3b52c54493f7121c02764ca13ad5
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-03-07 20:53:12 -0300

    como funciona o reduce

--- notes.js
+++ notes.js
@@ -1,5 +1,7 @@
 fetch (endpointDaAPI) //busca a API
 
+
+
 método forEach() executa uma função em cada elemento de um array
 //exemplo do uso do forEach substituindo o for 
 
@@ -15,6 +17,8 @@ livros.forEach(livro => {
     console.log(livro)
 })
 
+
+
 função map 
 /*o método map invoca uma função callback passada por argumento
 para cada elemento do array e devolve um novo array como resultado*/
@@ -26,6 +30,8 @@ const map1 = array1.map(x => x * 2);
 
 console.log(map1);
 //expected output : array [2, 8, 18, 32]
+
+
 
 função filter
 /*cria um novo array com todos os elementos que 
@@ -40,6 +46,8 @@ const podeDirigir = idades.filter(idade => {
 console.log (podeDirigir) //22,42,50
 console.log(idades) //10, 22, 42, 16, 50 n muda o array, 
 //cria um novo array com a resposta
+
+
 
 o método sort() ordena os elementos do próprio array e retorna o array. 
 A ordenação não é necessariamente estável. A ordenação padrão é de acordo 
@@ -59,3 +67,19 @@ obs: b - a = do maior para o menor
 a - b = do menor para o maior
 parâmetro a e b são a comparação dos arrays
 elemento undefined é classificado como último
+
+
+
+método reduce() executa uma função reducer (fornecida por você) 
+para cada elemento do array, resultando num único valor de retorno
+
+imagem map filter e reducer no google explica claramente o que cada função faz
+
+const precos = [10, 20, 70]
+
+const precoTotal = precos.reduce((acc, atual) => acc + atual) 
+
+console.log(precoTotal)
+
+para devolver o maior valor ex:
+const precoTotal = precos.reduce((acc, atual) => acc > atual? acc : atual)
